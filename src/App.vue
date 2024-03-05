0d6d200367b58bbfa17bf3e121e1a2cb2ec026b9
ChenNing <1915664627@qq.com>
2024-03-06 02:10:19 +0800

    feat: modelCard UI and back page scroll

--- src/App.vue
+++ src/App.vue
@@ -10,24 +10,24 @@
 // 设置暗色模式
 // document.documentElement.setAttribute('theme-mode', 'dark');
 
+
 export default {
   name: 'App',
-  components: {
-  },
+  components: {},
   methods: {
     handleResize() {
       var oldDisplayMobile = this.$store.getters.getDisplayMobile;
-      if (window.innerWidth < 600) 
+      if (window.innerWidth < 600)
         this.$store.commit('setDisplayMobile', true);
-      else 
+      else
         this.$store.commit('setDisplayMobile', false);
-      
-      if (oldDisplayMobile != this.$store.getters.getDisplayMobile) 
-        if(this.$store.getters.getDisplayMobile)
+
+      if (oldDisplayMobile != this.$store.getters.getDisplayMobile)
+        if (this.$store.getters.getDisplayMobile)
           this.$store.commit('workbenchSetPlacement', 'top');
-        else 
+        else
           this.$store.commit('workbenchSetPlacement', 'right');
-      
+
     }
   },
   mounted() {
@@ -37,6 +37,7 @@ export default {
   beforeDestroy() {
     window.removeEventListener('resize', this.handleResize);
   },
+
 }
 </script>
 
@@ -54,9 +55,8 @@ body {
   width: 100%;
   height: 100%;
   background-color: rgb(44, 44, 44);
-  background:
-    -webkit-linear-gradient(top, transparent 19px, #e7e7e7 20px),
-    -webkit-linear-gradient(left, transparent 19px, #e7e7e7 20px);
+  background: -webkit-linear-gradient(top, transparent 19px, #e7e7e7 20px),
+  -webkit-linear-gradient(left, transparent 19px, #e7e7e7 20px);
   background-size: 20px 20px;
   /* background:
     -webkit-linear-gradient(top, transparent 19px,  rgb(63, 63, 63) 20px),
