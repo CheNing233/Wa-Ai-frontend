932aaaa99607725e85d54944fc4c452415df1aab
ChenNing <1915664627@qq.com>
2024-03-11 15:23:12 +0800

    feat: tu cao login state transmit

--- src/pages/about/index.vue
+++ src/pages/about/index.vue
@@ -102,8 +102,17 @@
                   </t-popup>
 
                   <t-button
+                      v-if="item.jumpLink !== ''"
                       :href="item.jumpLink"
-                      :disabled="item.jumpLink === ''"
+                      target="_blank"
+                      variant="text" shape="square"
+                  >
+                    <LinkIcon/>
+                  </t-button>
+
+                  <t-button
+                      v-if="item.jumpMethod !== null"
+                      @click="item.jumpMethod"
                       target="_blank"
                       variant="text" shape="square"
                   >
@@ -126,6 +135,7 @@
 
 <script>
 import {LinkIcon, LogoGithubIcon, QrcodeIcon,} from 'tdesign-icons-vue';
+import CryptoJS from 'crypto-js';
 
 export default {
   name: 'about-index',
@@ -163,26 +173,88 @@ export default {
           desc: '810268559',
           qrLink: require('@/assets/about/group-qq-1.png'),
           qrVisible: false,
-          jumpLink: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=zN4TJHzGXB8cqGEjB-gX50HuT4zLD6Rp&authKey=1o3B9H2ahbKI1PogR6SQwPGfuqjVILFnLK6%2FN%2FC7EV24bPPWHCqkHXkhjUcQPT4v&noverify=0&group_code=810268559'
+          jumpLink: 'http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=zN4TJHzGXB8cqGEjB-gX50HuT4zLD6Rp&authKey=1o3B9H2ahbKI1PogR6SQwPGfuqjVILFnLK6%2FN%2FC7EV24bPPWHCqkHXkhjUcQPT4v&noverify=0&group_code=810268559',
+          jumpMethod: null
         },
         {
           title: 'WA-微信-交流1群',
           desc: 'WeChat',
           qrLink: require('@/assets/about/group-wc-1.png'),
           qrVisible: false,
-          jumpLink: ''
+          jumpLink: '',
+          jumpMethod: null
         },
         {
           title: '用户意见反馈服务平台',
-          desc: '便捷推荐，无需加群',
+          desc: '无需加群、扫码、换号，使用WA账号一键反馈',
           qrLink: null,
           qrVisible: false,
-          jumpLink: 'https://support.qq.com/product/639813'
+          // jumpLink: 'https://support.qq.com/product/639813',
+          jumpLink: '',
+          jumpMethod: this.handleTuCaoRequest
         }
       ]
     }
   },
-  methods: {},
+  methods: {
+    handleTuCaoRequest() {
+
+      const userInfo = this.$store.getters.userGetInfo;
+
+      if (userInfo === null) {
+        this.$message.error('请先登录到WA');
+        return;
+      }
+
+      function generateRandomString(length) {
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        let result = '';
+        const charactersLength = characters.length;
+        for (let i = 0; i < length; i++) {
+          result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        return result;
+      }
+
+      let data = {
+        "nickname": userInfo.nickName,
+        "avatar": userInfo.avatar || "https://tucao.qq.com/static/desktop/img/products/def-product-logo.png",
+        "openid": userInfo.id,
+        'nonce': generateRandomString(32),
+        'expired_at': '0'
+      };
+
+      const productId = 639813;
+
+      function encryptData(productId, productPrivateKey, data) {
+        // Convert data to JSON string
+        const dataStr = JSON.stringify(data);
+
+        // Pad key and IV to 16 bytes
+        const key = CryptoJS.enc.Utf8.parse(productPrivateKey.padEnd(16, '='));
+        const iv = CryptoJS.enc.Utf8.parse((productId + productPrivateKey).padEnd(16, '='));
+
+        // Encrypt using AES-128-CBC
+        const encrypted = CryptoJS.AES.encrypt(dataStr, key, {
+          iv: iv,
+          mode: CryptoJS.mode.CBC,
+          padding: CryptoJS.pad.Pkcs7
+        });
+
+        // Base64 encode the ciphertext
+        const encryptedBase64 = CryptoJS.enc.Base64.stringify(encrypted.ciphertext);
+
+        // Replace characters for URL safety
+        const encodedData = encryptedBase64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/, '');
+
+        return encodedData;
+      }
+
+      window.open(
+          'https://support.qq.com/product/' + productId + '?user_data=' + encryptData(productId, 'feTW0247', data)
+      );
+    }
+  },
   created() {
   }
 }
